590bc6f0e9b1c62d93b439634a9419ededc1f7d6
atakhadivi <atakhadivi@gmail.com>
2025-09-25 17:44:16 +0400

    add local ip

--- cashback_project/settings.py
+++ cashback_project/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-cashback-project-secret-key-123456789'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.121', '192.168.100.1']
 
 # Application definition
 INSTALLED_APPS = [
